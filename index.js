10316a8e0241d612757cf13cde41d927c8c4b958
Jorge Jimenez <jota@MacJota.local>
2020-02-21 14:15:16 -0300

    importacion y reindexacon

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
+global.confPath = __dirname + "/config.json";
+
 let downloader = false;
 let debug = false;
+let test = false;
+let reindexar = false;
+
 for (let i=2; i<process.argv.length; i++) {
     let arg = process.argv[i].toLowerCase();
     if (arg == "-d" || arg == "-download" || arg == "-downloader") downloader = true;
     if (arg == "-dbg" || arg == "-debug") debug = true;
+    if (arg == "-reindexar") reindexar = true;
 }
 if (!downloader && process.env.DOWNLOADER) {
     downloader = true;
+}
+if (process.env.REINDEXAR) {
+    reindexar = true;
 }
 if (debug) {
     process.env.DEBUG = true;
 }
 
-global.confPath = __dirname + "/config.json";
+if (test) {
+    const config = require("./lib/Config").getConfig();
+    let testFile = config.dataPath + "/downloads/test.grb2";
+    console.log("Iniciando importación de prueba de " + testFile);
+    const DescargaPronostico = require("./lib/DescargaPronostico");
+    const EjecucionModelo = require("./lib/EjecucionModelo");
+    const descargador = new DescargaPronostico(new EjecucionModelo(), 0);
+    descargador.importa(testFile)
+        .then(_ => console.log("Importación Finalizada"))
+        .catch(error => console.error(error));
+    return;
+}
+
+if (reindexar) {
+    const config = require("./lib/Config").getConfig();
+    const DescargaPronostico = require("./lib/DescargaPronostico");
+    const EjecucionModelo = require("./lib/EjecucionModelo");
+    const descargador = new DescargaPronostico(new EjecucionModelo(), 0);
+    console.log("Iniciando Reindexación completa");
+    descargador.fullReindexar()
+        .then(_ => console.log("Reindexación finalizada"))
+        .catch(error => console.error(error));
+    return;
+}
+
 const ProveedorCapasGFS4 = require("./lib/ProveedorCapasGFS4");
 
 if (downloader) {
